c222fde682653be7ef8e7d3cfff9fa9cf4d9981e
Antonio Matta <antonio.matta@telecom-paris.fr>
2026-02-04 14:47:28 +0100

    q1

--- quant.py
+++ quant.py
@@ -1,2 +1,49 @@
 #Fichier quantique
+
+#Q1 To C or not to C
+# THIS CELL IS PART OF THE SCORING CODE
+
+from qiskit.providers.fake_provider import GenericBackendV2
+from qiskit.visualization import plot_gate_map
+
+coupling_map = [
+    (0, 1),
+    (0, 2),
+    (2, 3),
+    (3, 4),
+    (1, 4),
+    (4, 5),
+    (7, 5),
+    (3, 6),
+    (6, 7),
+    (6, 8),
+    (8, 9),
+    (7, 10),
+    (9, 10),
+    (10, 11),
+    (11, 13),
+    (9, 12),
+    (12, 13),
+    (12, 14),
+    (15, 14),
+    (13, 16),
+    (15, 16),
+    (16, 17),
+    (17, 19),
+    (15, 18),
+    (18, 19),
+]
+coupling_map += [(y, x) for (x, y) in coupling_map]
+
+backend = GenericBackendV2(
+    num_qubits=20,
+    basis_gates=['id', 'rz', 'sx', 'x', 'cx', 'reset'],
+    coupling_map=coupling_map
+)
+plot_gate_map(backend)
+
+# SUBMISSION CELL
 from qiskit import QuantumCircuit
+
+qc = QuantumCircuit(20)
+# YOUR CODE HERE
